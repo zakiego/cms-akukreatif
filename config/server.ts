778e98847e426f1231a6510c7ec1fca01a01eca3
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-09-11 00:58:35 +0800

    feat: use VERCEL_URL for host

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,10 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("VERCEL_URL", "0.0.0.0") ?? env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 80),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
