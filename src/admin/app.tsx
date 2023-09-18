ef69bdabd0295a9cb616589b58a29adb8097857f
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-09-18 13:21:02 +0800

    feat: change admin title

---
+++ src/admin/app.tsx
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    locales: ["en"],
+    translations: {
+      en: {
+        "app.components.LeftMenu.navbrand.title": "Akukreatif Dashboard",
+      },
+    },
+  },
+
+  bootstrap(app) {
+    console.log(app);
+  },
+};
